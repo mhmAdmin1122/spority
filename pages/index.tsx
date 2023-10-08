60cb7fdeb9e355123e109fbc469f1e4730bc0f9e
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-10-08 22:54:30 +0500

    hero banner half code generated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout from "./Site/Components/Layout";
+import HeroSlick from "./Site/Components/HeroSlick";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <meta name="keywords" content="Live, Watch Sport, WorldCup Live, Watch Live Cricket, Football, Tenis" />
       </Head>
       <Layout>
+        <HeroSlick />
         Home Page ....
       </Layout>
     </>
