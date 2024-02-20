699bccf70fb1dab11438c65e62908544796c2f56
mhmAdmin1122 <hasaanad12min@gmail.com>
2024-02-20 23:52:11 +0500

    few more changing on events page

--- pages/Components/HeroBanners/EventsHero.tsx
+++ pages/Components/HeroBanners/EventsHero.tsx
@@ -18,14 +18,14 @@ const EventsHero = ({
 }: any) => {
   return (
     <div className="flex justify-center items-center w-full">
-      <Link href={`${Event1Link}`} title={Event1Name}>
-        <Image src={imageSrc1} alt={EventAltText1} />
+      <Link href={`${Event1Link}`} title={Event1Name} className="w-[33.3%]">
+        <Image src={imageSrc1} alt={EventAltText1} className="w-full" />
       </Link>
-      <Link href={`${Event2Link}`} title={Event2Name}>
-        <Image src={imageSrc2} alt={EventAltText2} />
+      <Link href={`${Event2Link}`} title={Event2Name} className="w-[33.3%]">
+        <Image src={imageSrc2} alt={EventAltText2} className="w-full" />
       </Link>
-      <Link href={`${Event3Link}`} title={Event3Name}>
-        <Image src={imageSrc3} alt={EventAltText3} />
+      <Link href={`${Event3Link}`} title={Event3Name} className="w-[33.3%]">
+        <Image src={imageSrc3} alt={EventAltText3} className="w-full" />
       </Link>
     </div>
   );
