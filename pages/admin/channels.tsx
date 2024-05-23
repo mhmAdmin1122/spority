cfda2612f29fdf95b8aaf260fcffee7d90175d47
Muhammad Hasaan Mumtaz <130135831+hasaanmumtaz6@users.noreply.github.com>
2024-05-23 14:43:07 +0000

    few more changes

--- pages/admin/channels.tsx
+++ pages/admin/channels.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Layout from './components/Layout'
-import Head from 'next/head'
-import ChannelCard from './components/ChannelCard'
+import React from "react";
+import Layout from "./components/Layout";
+import Head from "next/head";
+import ChannelCard from "./components/ChannelCard";
 
 const ChannelsPage = () => {
   return (
@@ -9,14 +9,15 @@ const ChannelsPage = () => {
       <Head>
         <title>Spority - Admin Channels</title>
       </Head>
-      <div className='relative flex flex-col gap-2'>
-        <div className='body-box-topBar-channels flex gap-8 justify-end w-full py-4 px-6 rounded-t-md'>
+      <div className="relative flex flex-col gap-2">
+        <button className="add-channel-btn-admin">Add Channels</button>
+        <div className="body-box-topBar-channels flex gap-8 justify-end w-full py-4 px-6 rounded-t-md">
           <h3>Traffic</h3>
           <h3>Live Watching</h3>
           <h3>Overview</h3>
           <h3>Action</h3>
         </div>
-        <div className='channels-list flex flex-col w-full gap-4 h-[430px] overflow-scroll overflow-x-hidden'>
+        <div className="channels-list flex flex-col w-full gap-4 h-[430px] overflow-scroll overflow-x-hidden">
           <ChannelCard />
           <ChannelCard />
           <ChannelCard />
@@ -24,7 +25,7 @@ const ChannelsPage = () => {
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default ChannelsPage
+export default ChannelsPage;
