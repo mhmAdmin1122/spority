21b8a7277ff7a9ab2d3bd0c679897c97d2bb168b
Muhammad Hasaan <114328181+mhmAdmin1122@users.noreply.github.com>
2024-05-29 19:10:12 +0000

    few more changings

--- pages/admin/components/PostForm.tsx
+++ pages/admin/components/PostForm.tsx
@@ -176,20 +176,9 @@ const PostForm = () => {
         </div>
       </div>
 
-      <div className="flex flex-col gap-3 w-[83%]">
-        <h2 className="text-left w-full text-xl font-bold">Video 1</h2>
-        <div className="bg-[#fff] px-6 py-3 w-full">
-          <input type="file" />
-        </div>
-      </div>
-      <div className="flex flex-col gap-3 w-[83%]">
-        <h2 className="text-left w-full text-xl font-bold">Video 2</h2>
-        <div className="bg-[#fff] px-6 py-3 w-full">
-          <input type="file" />
-        </div>
-      </div>
-
-      <button type="submit" className="bg-colored-post-btn px-5 py-2">Submit Now</button>
+      <button type="submit" className="bg-colored-post-btn px-5 py-2">
+        Submit Now
+      </button>
     </form>
   );
 };
