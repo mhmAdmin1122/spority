ae0b37d6f7307f3fb910cb90448ac50e92f93b51
Muhammad Hasaan <114328181+mhmAdmin1122@users.noreply.github.com>
2024-05-30 12:53:40 +0000

    new chnage

--- pages/Live-Channels/index.tsx
+++ pages/Live-Channels/index.tsx
@@ -14,12 +14,23 @@ const LiveChannelsPage = () => {
       </Head>
       <div className="flex items-start gap-4 justify-center p-5 w-full">
         <div className="player-of-main-channel flex items-center justify-center w-[70%]">
-          <iframe
-            src={`https://player002.vip/embed2.php?id=asports`}
+          {/* <iframe
+            src={`https://player003.vip/embed2.php?id=asports&q=A Sports`}
             scrolling="no"
             allowFullScreen
             className="w-[100%] h-[500px] rounded-md overflow-hidden"
-          />
+          /> */}
+          <iframe
+            frameBorder="0"
+            height="500"
+            src="https://player003.vip/embed2.php?id=willow"
+            name="iframe_a"
+            scrolling="no"
+            width="640"
+            className="w-[100%] h-[500px] rounded-md overflow-hidden"
+          >
+            Your Browser Do not Support Iframe
+          </iframe>
         </div>
         <div className="overflow-scroll overflow-x-hidden listof-event max-h-[460px] min-h-[460px]">
           <EventsCards />
