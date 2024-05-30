33b82f211d6b579662e44e6a6bd2d8ec7ddcbb55
Muhammad Hasaan <114328181+mhmAdmin1122@users.noreply.github.com>
2024-05-30 12:10:49 +0000

    few more changings

---
+++ pages/Components/SectionsCards/EventsCards.tsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+const EventsCards = () => {
+  const [apiData, setApiData] = useState<any>([]);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    axios.get("/api/event").then((res: any) => {
+      const data = res.data;
+      setApiData(data);
+      setLoader(false);
+    });
+  }, []);
+
+  const event = apiData?.map((user: any) => user);
+  return (
+    <div className="w-full flex flex-col justify-center">
+      {loader ? (
+        <div className="flex justify-center w-full items-center flex-col">
+          Loading....
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4 justify-center h-[430px] overflow-scroll overflow-x-hidden events-list">
+          {event?.map((event: any) => (
+            <div
+              key={event?._id}
+              className="w-[260px] h-[160px] object-cover overflow-hidden border-[1px] border-gray-200 rounded-lg bg-[#0C0501] text-black flex flex-col hover:shadow-md hover:shadow-gray-400 relative transition-all duration-700 ease-in-out event-card"
+            >
+              <Image
+                src={event?.picture}
+                alt={`${event?.name}`}
+                title={`${event?.name}`}
+                width={120}
+                height={90}
+                className="w-full h-full"
+              />
+              <Link
+                href={event?.eventchannel1}
+                className="border-[1px] border-gray-200 rounded-sm px-3 py-1 absolute z-30 bg-[#00ff] text-[#fff] watch-now-btn"
+              >
+                Watch Now
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EventsCards;
--- pages/Live-Channels/index.tsx
+++ pages/Live-Channels/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import Layout from '../Components/Layout'
-import Head from 'next/head'
-import Channels from '../Components/Sections/Channels'
-import Pricing from '../Components/Sections/Pricing'
-import NewsLatter from '../Components/Sections/NewsLatter'
+import React from "react";
+import Layout from "../Components/Layout";
+import Head from "next/head";
+import Channels from "../Components/Sections/Channels";
+import Pricing from "../Components/Sections/Pricing";
+import NewsLatter from "../Components/Sections/NewsLatter";
+import EventsCards from "../Components/SectionsCards/EventsCards";
 
 const LiveChannelsPage = () => {
   return (
@@ -11,11 +12,25 @@ const LiveChannelsPage = () => {
       <Head>
         <title>Live Channels - Spority</title>
       </Head>
+      <div className="flex items-start gap-4 justify-center p-5 w-full">
+        <div className="player-of-main-channel flex items-center justify-center w-[70%]">
+          <iframe
+            src={`https://player002.vip/embed2.php?id=asports`}
+            scrolling="no"
+            allowFullScreen
+            className="w-[100%] h-[500px] rounded-md overflow-hidden"
+          />
+        </div>
+        <div className="overflow-scroll overflow-x-hidden listof-event max-h-[460px] min-h-[460px]">
+          <EventsCards />
+        </div>
+      </div>
+      {/*       
       <Channels />
       <Pricing />
-      <NewsLatter />
+      <NewsLatter /> */}
     </Layout>
-  )
-}
+  );
+};
 
-export default LiveChannelsPage
+export default LiveChannelsPage;
