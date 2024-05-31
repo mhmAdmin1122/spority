061b75a9a371407a666e8e60bf1b9f470264680c
Muhammad Hasaan <114328181+mhmAdmin1122@users.noreply.github.com>
2024-05-31 13:45:23 +0000

    Live channel page channel url problem solved

--- pages/Live-Channels/index.tsx
+++ pages/Live-Channels/index.tsx
@@ -14,16 +14,10 @@ const LiveChannelsPage = () => {
       </Head>
       <div className="flex items-start gap-4 justify-center p-5 w-full">
         <div className="player-of-main-channel flex items-center justify-center w-[70%]">
-          {/* <iframe
-            src={`https://player003.vip/embed2.php?id=asports&q=A Sports`}
-            scrolling="no"
-            allowFullScreen
-            className="w-[100%] h-[500px] rounded-md overflow-hidden"
-          /> */}
           <iframe
             frameBorder="0"
             height="500"
-            src="https://player003.vip/embed2.php?id=willow"
+            src="//stream.crichd.vip/update/skys2.php"
             name="iframe_a"
             scrolling="no"
             width="640"
