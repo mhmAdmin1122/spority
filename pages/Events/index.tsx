a003aec5abaec2856e7da52c4b0212e0d618ff4f
Muhammad Hasaan <114328181+mhmAdmin1122@users.noreply.github.com>
2024-06-01 23:13:40 +0000

    Few more changings on events page

--- pages/Events/index.tsx
+++ pages/Events/index.tsx
@@ -3,31 +3,18 @@ import Layout from "../Components/Layout";
 import Head from "next/head";
 import Channels from "../Components/Sections/Channels";
 import EventsHero from "../Components/HeroBanners/EventsHero";
-import eventpic1 from "@/public/img/events2.svg";
-import Image from "next/image";
-import eventpic3 from "@/public/img/events3.svg";
-import Slider from "react-slick";
-import WatchBtn from "../Components/WatchBtn";
 import FeaturedEvents from "../Components/SectionsCards/FeaturedEvents";
 import MostViewEvents from "../Components/SectionsCards/MostViewEvents";
+import NewsLatter from "../Components/Sections/NewsLatter";
 
 const EventsPage = () => {
-  const settings = {
-    dots: true,
-    infinite: false,
-    autoplay: true,
-    speed: 4000,
-    autoplaySpeed: 4000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
   return (
     <Layout>
       <Head>
         <title>Events - Spority</title>
       </Head>
       <EventsHero />
-      <div className="fetured-events-most-view-events bg-[#0e0805] text-white px-20 pt-20 pb-40">
+      <div className="fetured-events-most-view-events bg-[#0e0805] text-white px-20 pt-24 pb-5">
         <h3 className="text-4xl">Featured Events</h3>
         <div className="flex items-center justify-center gap-4 w-full py-8">
           <div className="featured-Events-post-eventpage flex items-center justify-center w-[50%] flex-wrap gap-4">
@@ -42,7 +29,7 @@ const EventsPage = () => {
         </div>
       </div>
       <Channels />
-      {/* <Pricing /> */}
+      <NewsLatter />
     </Layout>
   );
 };
