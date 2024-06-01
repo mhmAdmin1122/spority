ef9cdc8fbce848879927250437e5d05f5e29f3f7
Muhammad Hasaan <114328181+mhmAdmin1122@users.noreply.github.com>
2024-06-01 22:13:32 +0000

    slider for desktop of event age added

--- pages/Events/index.tsx
+++ pages/Events/index.tsx
@@ -4,17 +4,52 @@ import Head from "next/head";
 import Channels from "../Components/Sections/Channels";
 import Pricing from "../Components/Sections/Pricing";
 import EventsHero from "../Components/HeroBanners/EventsHero";
-import eventpic1 from '@/public/img/events2.svg'
-import eventpic2 from '@/public/img/events1.svg'
-import eventpic3 from '@/public/img/events3.svg'
+import eventpic1 from "@/public/img/events2.svg";
+import Image from "next/image";
+import eventpic3 from "@/public/img/events3.svg";
+import Slider from "react-slick";
 
 const EventsPage = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <Layout>
       <Head>
         <title>Events - Spority</title>
       </Head>
-      <EventsHero
+
+      <div className="event-page-hero-banner w-full overflow-hidden h-[80vh]">
+        <div className="slider-container w-full h-[80vh] overflow-hidden">
+          <Slider {...settings}>
+            <div className="object-cover w-full h-full">
+              <Image src={eventpic3} alt="image-of-banner" className="object-fill w-full" />
+            </div>
+            <div className="object-cover w-full h-full">
+              <Image src={eventpic1} alt="image-of-banner" className="object-cover w-full" />
+            </div>
+            <div className="object-cover w-full h-full">
+              <Image src={eventpic3} alt="image-of-banner" className="object-cover w-full" />
+            </div>
+            <div className="object-cover w-full h-full">
+              <Image src={eventpic1} alt="image-of-banner" className="object-cover w-full" />
+            </div>
+            <div className="object-cover w-full h-full">
+              <Image src={eventpic3} alt="image-of-banner" className="object-cover w-full" />
+            </div>
+            <div className="object-cover w-full h-full">
+              <Image src={eventpic1} alt="image-of-banner" className="object-cover w-full" />
+            </div>
+          </Slider>
+        </div>
+      </div>
+      {/* <EventsHero
         imageSrc1={eventpic1}
         imageSrc2={eventpic2}
         imageSrc3={eventpic3}
@@ -27,7 +62,7 @@ const EventsPage = () => {
         EventAltText1="psl-match-pic"
         EventAltText2="ipl-match-pic"
         EventAltText3="bpl-match-pic"
-      />
+      /> */}
       <Channels />
       <Pricing />
     </Layout>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/admin.css";
 import "@/styles/buttons.css";
 import "@/styles/Newslatter.css";
 import "@/styles/channels.css";
+import "@/styles/sliders.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import type { AppProps } from "next/app";
