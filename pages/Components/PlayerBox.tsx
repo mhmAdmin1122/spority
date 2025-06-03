8a9449b6615648a1a73b9db201431c4b041108be
Muhammad Hasaan <114328181+mhmAdmin1122@users.noreply.github.com>
2025-06-03 22:09:54 +0500

    Update PlayerBox.tsx
    jk

--- pages/Components/PlayerBox.tsx
+++ pages/Components/PlayerBox.tsx
@@ -6,7 +6,7 @@ const PlayerBox = () => {
     <div className="flex items-center gap-4 justify-center px-5 pb-5 pt-20 w-full bg-[#0e0805] player-box-live-channel-page">
       <div className="player-of-main-channel flex items-center justify-center w-[70%]">
         <iframe
-          src="//stream.crichd.vip/update/skys2.php"
+          src="//stream.crichd.sc/update/star.php"
           name="iframe_a"
           scrolling="no"
           allowFullScreen
